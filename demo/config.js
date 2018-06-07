276337158156fe6f63e6a101d3e72b5b929b2869
yleo77 <ylep77@gmail.com>
2018-06-07 23:38:40 +0800

    feat: improve demo

--- demo/config.js
+++ demo/config.js
@@ -1,11 +1,34 @@
 module.exports = {
   middleware: [
+
     'outer',
     'timing',
+
     'maius:router', // reordered middleware.
+
+    // koa-middleware example with simple example
+    'kcors',
+
+    // koa-middleware example with complex args
+    {
+      name: 'koa-morgan',
+      args: [
+        'combined',
+        {
+          skip: function (req, res) {
+            return false;
+          }
+        }
+      ],
+    },
+
+    // middleware with `afterRouter` option
     {
       name: 'after',
-      args: [{name: 'hihao'}],
+      args: [{
+        name: 'nihao'
+      }],
+      afterRouter: true,
     },
   ],
   static: { },
